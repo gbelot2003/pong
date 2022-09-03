f74b7413f45d563e6abdfbcdb45343e63f6c930e
Gerardo Belot <gbelot2003@hotmail.com>
2022-09-03 15:52:14 -0600

    🔧 Configurando fisicas del juego

--- game/src/scenes/MainScene.ts
+++ game/src/scenes/MainScene.ts
@@ -1,15 +1,25 @@
 import { buildTargets } from '../constants';
+import Palas from './Palas';
 
 export class MainScene extends Phaser.Scene {
     image: Phaser.GameObjects.Image;
+    izquiera: any;
+    derecha: any;
+    bola : any;
+
+
     constructor() {
         super({
-            key: 'MainScene'
+            key: 'MainScene',
+            physics: {
+                default: 'arcade',
+            }
         });
     }
 
     init() {
         console.log('MainScene');
+        
     }
 
     create(): void {
@@ -17,19 +27,48 @@ export class MainScene extends Phaser.Scene {
         let middle: number;
         let width : any = this.sys.game.config.width;
         let height : any = this.sys.game.config.height;
+        
         center = width / 2;
         middle = height / 2;
 
         // Separador
         let separador = this.add.image(center, 200, "separator")
 
-        // Palas
-        let izquiera = this.add.image(30, middle, "right_pallete")
-        let derecha = this.add.image(610, middle, "left_pallete")
+        // Palas, como clase
+        this.izquiera = new Palas(this, 30, middle, "right_pallete");
+        this.derecha = new Palas(this, 610, middle, "left_pallete");
+
 
         //bola
-        let bola = this.physics.add.image(center, middle, "ball")
+        this.bola = this.physics.add.image(center, middle, "ball")
 
+        //fisicas
+        this.bola.setVelocity(-180);
+        this.bola.setCollideWorldBounds();
+        this.physics.world.setBoundsCollision(false, false, true, true);
 
+        this.bola.setBounce(1);
+        this.physics.add.collider(this.bola, this.izquiera, chocaPala(this.bola), null)
+        this.physics.add.collider(this.bola, this.derecha, chocaPala(this.bola), null)
+
+        function chocaPala(bola) : any {
+            bola.setVelocityY(Phaser.Math.Between(-120, 120))
+            return ;
+        }
     }
+
+    update(): void {
+        let center: number;
+        let middle: number;
+        let width : any = this.sys.game.config.width;
+        let height : any = this.sys.game.config.height;
+        
+        center = width / 2;
+        middle = height / 2;
+
+        if(this.bola.x < 0 || this.bola.x > width){
+            this.bola.setPosition(center, middle);
+        }
+    }
+    
 }
